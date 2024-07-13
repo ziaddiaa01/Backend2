ed1600462d66dde9738595bdc3adef5958736bfa
ziaddiaa01 <98720925+ziaddiaa01@users.noreply.github.com>
2024-07-14 02:55:39 +0300

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:DB/connection.js
 import mongoose from "mongoose"
 
 const connectDB = async()=> {
@@ -11,20 +10,3 @@ const connectDB = async()=> {
         }
     );
 }
-
-=======
-import mongoose from "mongoose"
-
-const connectDB = async()=> {
-    return await mongoose.connect(`mongodb+srv://gmalak541:pSCxIZpuYqKnCB6N@greenwave.rsffrse.mongodb.net/GreenWave?retryWrites=true&w=majority&appName=GreenWave`)
-    .then(res=>{
-        console.log(`DB Connected Successfully.........`);
-    }).catch(
-        err=>{
-            console.log(`Failed Connection: ${err}`);
-        }
-    );
-}
-
->>>>>>> cb57dd93db784ee05bab7bdcb983cbf62735e45f:BackEnd/DB/connection.js
-export default connectDB
